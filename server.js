f854bd6354139bf13e1f8d0fe2f58474874ec111
use-go-to <54232215+use-go-to@users.noreply.github.com>
2025-04-17 09:04:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,101 @@ const io = socketIo(server, {
     }
 });
 
-// ... (le reste de votre code Socket.io reste inchangé)
+// Stockage des données de groupe
+const groups = {};
+
+io.on('connection', (socket) => {
+    console.log('Nouvelle connexion:', socket.id);
+    
+    let currentGroup = null;
+    let currentUserId = null;
+    
+    // Rejoindre un groupe
+    socket.on('joinGroup', (groupId, userId) => {
+        currentGroup = groupId;
+        currentUserId = userId;
+        
+        if (!groups[groupId]) {
+            groups[groupId] = {
+                members: {},
+                currentMovie: null,
+                votes: {}
+            };
+        }
+        
+        groups[groupId].members[userId] = {
+            id: userId,
+            joinedAt: new Date()
+        };
+        
+        socket.join(groupId);
+        updateGroup(groupId);
+    });
+    
+    // Voter pour un film
+    socket.on('groupVote', (groupId, userId, movieId, vote) => {
+        if (!groups[groupId]) return;
+        
+        groups[groupId].votes[userId] = {
+            movieId,
+            vote,
+            timestamp: new Date()
+        };
+        
+        checkConsensus(groupId);
+        updateGroup(groupId);
+    });
+    
+    // Déconnexion
+    socket.on('disconnect', () => {
+        if (currentGroup && currentUserId && groups[currentGroup]) {
+            delete groups[currentGroup].members[currentUserId];
+            delete groups[currentGroup].votes[currentUserId];
+            
+            if (Object.keys(groups[currentGroup].members).length === 0) {
+                delete groups[currentGroup];
+            } else {
+                updateGroup(currentGroup);
+            }
+        }
+    });
+    
+    // Mettre à jour les données du groupe
+    function updateGroup(groupId) {
+        if (!groups[groupId]) return;
+        
+        io.to(groupId).emit('groupUpdate', {
+            members: groups[groupId].members,
+            movie: groups[groupId].currentMovie,
+            votes: groups[groupId].votes
+        });
+    }
+    
+    // Vérifier si un film fait l'unanimité
+    function checkConsensus(groupId) {
+        const group = groups[groupId];
+        if (!group) return;
+        
+        const votes = Object.values(group.votes);
+        const members = Object.keys(group.members);
+        
+        if (votes.length < members.length) return;
+        
+        const voteCounts = {};
+        votes.forEach(v => {
+            if (v.vote === 'like') {
+                voteCounts[v.movieId] = (voteCounts[v.movieId] || 0) + 1;
+            }
+        });
+        
+        for (const movieId in voteCounts) {
+            if (voteCounts[movieId] === members.length) {
+                io.to(groupId).emit('consensusReached', movieId);
+                return;
+            }
+        }
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
